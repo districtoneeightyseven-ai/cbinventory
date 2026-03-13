b3d9e6dde0b3f8c936e5ba8c1bbf38ac8eb98218
districtoneeightyseven-ai <districtoneeightyseven@gmail.com>
2026-03-13 04:03:03 -0400

    Create fxmanifest.lua for QBCore inventory script

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'YourName'
+description 'QBCore Inventory Script'
+version '1.0.0'
+
+--[[
+  This is a QBCore inventory script for managing items.
+]]--
+
+client_script 'client.lua'
+server_script 'server.lua'
